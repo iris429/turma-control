c30052e9bd48cb9ce2798cbe6a1d8a5b4245ef6b
iris <irismararaujo2004@gmail.com>
2026-05-06 13:59:17 -0300

    criando tela inicial

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-console.log('rodando')
+const tela = require("readline-sync")
+
+function main() { 
+	let loop = true
+
+	while(loop) { 
+	console.log("1 - Cadastrar aluno\n2 - Adicionar Notas\n 3 - Listar\n 0 - Sair") 
+	const opção:string = tela.question("Deseja continuar? ")
+	if(opção === "0") loop = false
+	}
+}
+
+main() 
+
+
+
+
